5256970003e4f994ec13667ff8192a7d0d7da60a
Damotsoff <george.damotsov@gmail.com>
2023-02-18 23:05:13 +0300

    add write to redis

--- mail.py
+++ mail.py
@@ -1,7 +1,10 @@
 import json
+import redis
 from kafka import KafkaConsumer
-from config import ORDER_CONFIRMED_KAFKA_TOPIC,KAFKA_HOST
+from config import ORDER_CONFIRMED_KAFKA_TOPIC, KAFKA_HOST, REDIS_HOST, REDIS_PORT
 
+pool = redis.ConnectionPool(host=REDIS_HOST, port=REDIS_PORT, db=0)
+gmail_database = redis.Redis(connection_pool=pool)
 ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
 consumer = KafkaConsumer(ORDER_CONFIRMED_KAFKA_TOPIC, bootstrap_servers=KAFKA_HOST)
 
@@ -12,7 +15,9 @@ while True:
     for message in consumer:
         consumed_message = json.loads(message.value.decode())
         customer_email = consumed_message["customer_email"]
+        user_id = consumed_message["user_id"]
         print(f"Sending email to {customer_email} ")
         emails_sent_so_far.add(customer_email)
         print(f"So far emails sent to {len(emails_sent_so_far)} unique emails")
-
+        gmail_database.set(user_id,customer_email)
+        print("Add to db!")
